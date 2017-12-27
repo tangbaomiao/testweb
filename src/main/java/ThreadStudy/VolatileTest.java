0e1dc759636cd491681d1cfa14ab1f45d745ab68
tangbaomiao <1044966027@qq.com>
2017-12-27 13:44:19 +0800

    thread study1

--- src/main/java/ThreadStudy/VolatileTest.java
+++ src/main/java/ThreadStudy/VolatileTest.java
@@ -6,6 +6,7 @@ package ThreadStudy;
  * 事实上运行它会发现每次运行结果都不一致，都是一个小于10000的数字。
  * https://www.cnblogs.com/dolphin0520/p/3920373.html   解释在这个链接深入剖析部分
  * 为什么在一个线程修改inc 不会把其他线程的inc置为无效？？？？？？
+ *
  */
 public class VolatileTest {
     public volatile int inc = 0;
